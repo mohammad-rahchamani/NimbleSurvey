1b0665f55e48d3d821cedd742aa29e035f9f9e19
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-05-03 12:57:04 +0430

    create new file for network handler tests

--- Nimble SurveyTests/NetworkHandlerTests.swift
+++ Nimble SurveyTests/NetworkHandlerTests.swift
@@ -1,14 +1,13 @@
 //
-//  Nimble_SurveyTests.swift
+//  NetworkHandlerTests.swift
 //  Nimble SurveyTests
 //
 //  Created by Mohammad Rahchamani on 5/3/22.
 //
 
 import XCTest
-@testable import Nimble_Survey
 
-class Nimble_SurveyTests: XCTestCase {
+class NetworkHandlerTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
