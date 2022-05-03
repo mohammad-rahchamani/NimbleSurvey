c21597e805a0838ffda828b4c808baa163e8f41d
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-05-03 14:57:20 +0430

    network handler fails on all cases of invalid response

--- Nimble SurveyTests/NetworkHandlerTests.swift
+++ Nimble SurveyTests/NetworkHandlerTests.swift
@@ -73,6 +73,8 @@ class URLProtocolStub: URLProtocol {
 
 class NetworkHandler {
     
+    private static let VALID_STATUS_CODES = 200...299
+    
     private let session: URLSession
     
     init(session: URLSession) {
@@ -81,9 +83,34 @@ class NetworkHandler {
     
     func load(request: URLRequest,
               completion: @escaping (Result<Data, Error>) -> ()) {
-        session.dataTask(with: request) {_,_,_ in
+        session.dataTask(with: request) { data, response, error in
+            
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data,
+                  NetworkHandler.isResponseValid(response),
+                  !data.isEmpty else {
+                      completion(.failure(NetworkHandlerError.invalidData))
+                      return
+            }
+            
+            completion(.success(data))
             
         }.resume()
+    }
+    
+    private static func isResponseValid(_ response: URLResponse?) -> Bool {
+        guard let response = response as? HTTPURLResponse else {
+            return false
+        }
+        return VALID_STATUS_CODES.contains(response.statusCode)
+    }
+    
+    private enum NetworkHandlerError: Error {
+        case invalidData
     }
     
 }
@@ -123,14 +150,121 @@ class NetworkHandlerTests: XCTestCase {
         XCTAssertEqual(capturedRequest, expectedRequest)
     }
     
+    func test_load_failsOnNetworkError() {
+        let sut = makeSUT()
+        assert(sut,
+               loads: anyRequest(),
+               receives: .failure(anyNSError()),
+               stubbing: (nil, nil, anyNSError()))
+    }
+    
+    func test_load_failsOnErrorAndResponse() {
+        let sut = makeSUT()
+        assert(sut,
+               loads: anyRequest(),
+               receives: .failure(anyNSError()),
+               stubbing: (nil, httpResponse(for: anyURL(), withStatus: 200), anyNSError()))
+    }
+    
+    func test_load_failsOnErrorAndData() {
+        let sut = makeSUT()
+        assert(sut,
+               loads: anyRequest(),
+               receives: .failure(anyNSError()),
+               stubbing: (Data(), nil, anyNSError()))
+    }
+    
+    func test_load_failsOnErrorReponseAndData() {
+        let sut = makeSUT()
+        assert(sut,
+               loads: anyRequest(),
+               receives: .failure(anyNSError()),
+               stubbing: (Data(), httpResponse(for: anyURL(), withStatus: 200), anyNSError()))
+    }
+    
+    func test_load_failsOnResponseWithoutData() {
+        let sut = makeSUT()
+        assert(sut,
+               loads: anyRequest(),
+               receives: .failure(anyNSError()),
+               stubbing: (nil, httpResponse(for: anyURL(), withStatus: 200), nil))
+    }
+    
+    func test_load_failsOnDataWithoutResponse() {
+        let sut = makeSUT()
+        assert(sut,
+               loads: anyRequest(),
+               receives: .failure(anyNSError()),
+               stubbing: (Data(), nil, nil))
+    }
+    
+    func test_load_failsOnNoDataNoResponseNoError() {
+        let sut = makeSUT()
+        assert(sut,
+               loads: anyRequest(),
+               receives: .failure(anyNSError()),
+               stubbing: (nil, nil, nil))
+    }
+    
     // MARK: - helpers
     
     func makeSUT() -> NetworkHandler {
         NetworkHandler(session: .shared)
     }
     
+    func assert(_ sut: NetworkHandler,
+                loads request: URLRequest,
+                receives expectedResult: Result<Data, Error>,
+                stubbing stub: (Data?, URLResponse?, Error?),
+                file: StaticString = #filePath,
+                line: UInt = #line) {
+        URLProtocolStub.stub(withData: stub.0,
+                             response: stub.1,
+                             error: stub.2)
+        expect(sut,
+               toLoad: request,
+               withResult: expectedResult,
+               file: file,
+               line: line)
+    }
+    
+    func expect(_ sut: NetworkHandler,
+                toLoad request: URLRequest,
+                withResult expectedResult: Result<Data, Error>,
+                file: StaticString = #filePath,
+                line: UInt = #line) {
+        let exp = XCTestExpectation(description: "waiting for load completion")
+        sut.load(request: request) { result in
+            switch (result, expectedResult) {
+            case (.failure, .failure):
+                ()
+            case (.success(let data), .success(let expectedData)):
+                XCTAssertEqual(data, expectedData, file: file, line: line)
+            default:
+                XCTFail("expected \(expectedResult) got \(result)", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func anyURL() -> URL {
+        URL(string: "https://any-url.com")!
+    }
+    
     func anyRequest() -> URLRequest {
-        URLRequest(url: URL(string: "https://any-url.com")!)
+        URLRequest(url: anyURL())
+    }
+    
+    func anyNSError() -> NSError {
+        NSError(domain: "test domain", code: 0, userInfo: nil)
+    }
+    
+    func httpResponse(for url: URL, withStatus status: Int) -> HTTPURLResponse? {
+        HTTPURLResponse(url: url,
+                        statusCode: status,
+                        httpVersion: nil,
+                        headerFields: nil)
     }
 
 }
