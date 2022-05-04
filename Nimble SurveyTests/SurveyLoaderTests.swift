970941be7bce19a4476c6b5a2c985acb024debb2
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-05-04 16:06:19 +0430

    survey loader does not send request upon creation

--- Nimble SurveyTests/SurveyLoaderTests.swift
+++ Nimble SurveyTests/SurveyLoaderTests.swift
@@ -6,30 +6,26 @@
 //
 
 import XCTest
+import Nimble_Survey
+
+public class SurveyLoader {
+    
+    let loader: RequestLoader
+    
+    init(loader: RequestLoader) {
+        self.loader = loader
+    }
+    
+    
+    
+}
 
 class SurveyLoaderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_init_doesNotMessageLoader() {
+        let spy = RequestLoaderSpy()
+        _ = SurveyLoader(loader: spy)
+        XCTAssertTrue(spy.messages.isEmpty)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
