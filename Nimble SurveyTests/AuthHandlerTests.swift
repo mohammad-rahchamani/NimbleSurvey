debc0f587667a4bc145e457bf150c1e69ef2707d
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-05-05 13:47:42 +0430

    login requests login from service

--- Nimble SurveyTests/AuthHandlerTests.swift
+++ Nimble SurveyTests/AuthHandlerTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import Nimble_Survey
 
-public class AuthHandler {
+public class AuthHandler: AuthService {
     
     private let service: AuthService
     private let store: TokenStore
@@ -17,20 +17,80 @@ public class AuthHandler {
         self.service = service
         self.store = store
     }
+    public func login(withEmail email: String,
+                      andPassword password: String,
+                      completion: @escaping (Result<AuthToken, Error>) -> ()) {
+        service.login(withEmail: email,
+                      andPassword: password) { [weak self] result in
+            guard let self = self else { return }
+            if let token = try? result.get() {
+                self.store.save(token: token)
+            }
+            completion(result)
+        }
+    }
+    
+    public func register(withEmail email: String,
+                         password: String, passwordConfirmation: String,
+                         completion: @escaping (Result<(), Error>) -> ()) {
+        service.register(withEmail: email,
+                         password: password,
+                         passwordConfirmation: passwordConfirmation,
+                         completion: completion)
+    }
+    
+    public func logout(token: String, completion: @escaping (Result<(), Error>) -> ()) {
+        store.delete()
+        service.logout(token: token, completion: completion)
+    }
+    
+    public func forgotPassword(email: String, completion: @escaping (Result<String, Error>) -> ()) {
+        service.forgotPassword(email: email,
+                               completion: completion)
+    }
+    
+    public func refreshToken(token: String, completion: @escaping (Result<AuthToken, Error>) -> ()) {
+        service.refreshToken(token: token) { result in
+            if let token = try? result.get() {
+                self.store.save(token: token)
+            }
+            completion(result)
+        }
+    }
+    
     
 }
 
 class AuthHandlerTests: XCTestCase {
     
-    func test_handler_doesNotMessageServiceAndStore() {
-        
-        let serviceSpy = AuthServiceSpy()
-        let storeSpy = TokenStoreSpy()
-        let _ = AuthHandler(service: serviceSpy, store: storeSpy)
-        
+    func test_init_doesNotMessageServiceAndStore() {
+        let (serviceSpy, storeSpy, _) = makeSUT()
         XCTAssertTrue(serviceSpy.messages.isEmpty)
         XCTAssertTrue(storeSpy.messages.isEmpty)
-        
     }
-
+    
+    func test_login_requestsLoginFromService() {
+        let (serviceSpy, storeSpy, sut) = makeSUT()
+        
+        let email = "email"
+        let password = "password"
+        sut.login(withEmail: email,
+                  andPassword: password) { _ in }
+        
+        XCTAssertEqual(serviceSpy.messages.first, AuthServiceSpy.Message.login(email: email,
+                                                                               password: password))
+        XCTAssertTrue(storeSpy.messages.isEmpty)
+    }
+    
+    // MARK: - helpers
+    
+    func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (AuthServiceSpy, TokenStoreSpy, AuthHandler) {
+        let serviceSpy = AuthServiceSpy()
+        let storeSpy = TokenStoreSpy()
+        let sut = AuthHandler(service: serviceSpy, store: storeSpy)
+        trackForMemoryLeak(serviceSpy, file: file, line: line)
+        trackForMemoryLeak(storeSpy, file: file, line: line)
+        trackForMemoryLeak(sut, file: file, line: line)
+        return (serviceSpy, storeSpy, sut)
+    }
 }
