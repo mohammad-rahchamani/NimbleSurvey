fd65871dd19b6aec79091391ecb4c9a41759609d
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-05-05 09:55:06 +0430

    add secure store to keep auth token

---
+++ Nimble Survey/Auth/Store/TokenStore.swift
@@ -0,0 +1,48 @@
+//
+//  TokenStore.swift
+//  Nimble Survey
+//
+//  Created by Mohammad Rahchamani on 5/5/22.
+//
+
+import Foundation
+import SwiftKeychainWrapper
+
+public final class TokenStore {
+    
+    private let key = "NimbleAuthToken"
+    private let keychain: KeychainWrapper
+    
+    public init(keychain: KeychainWrapper = .standard) {
+        self.keychain = keychain
+    }
+    
+    public func load() -> AuthToken? {
+        let data = keychain.data(forKey: key)
+        guard let data = data else {
+            return nil
+        }
+        return decode(data)
+    }
+    
+    public func save(token: AuthToken) {
+        guard let data = encode(token) else { return }
+        keychain.set(data,
+                                     forKey: key)
+    }
+    
+    public func delete() {
+        keychain.removeObject(forKey: key)
+    }
+    
+    private func encode(_ token: AuthToken) -> Data? {
+        let encoder = JSONEncoder()
+        return try? encoder.encode(token)
+    }
+    
+    private func decode(_ data: Data) -> AuthToken? {
+        let decoder = JSONDecoder()
+        return try? decoder.decode(AuthToken.self, from: data)
+    }
+    
+}
