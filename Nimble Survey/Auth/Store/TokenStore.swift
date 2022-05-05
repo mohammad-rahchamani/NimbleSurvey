b7e3b6a7b1c630673474ab01294963fbb6bffec1
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-05-05 09:58:51 +0430

    extract token store protocol to create spy for testing

--- Nimble Survey/Auth/Store/TokenStore.swift
+++ Nimble Survey/Auth/Store/TokenStore.swift
@@ -6,43 +6,11 @@
 //
 
 import Foundation
-import SwiftKeychainWrapper
 
-public final class TokenStore {
+public protocol TokenStore {
     
-    private let key = "NimbleAuthToken"
-    private let keychain: KeychainWrapper
-    
-    public init(keychain: KeychainWrapper = .standard) {
-        self.keychain = keychain
-    }
-    
-    public func load() -> AuthToken? {
-        let data = keychain.data(forKey: key)
-        guard let data = data else {
-            return nil
-        }
-        return decode(data)
-    }
-    
-    public func save(token: AuthToken) {
-        guard let data = encode(token) else { return }
-        keychain.set(data,
-                                     forKey: key)
-    }
-    
-    public func delete() {
-        keychain.removeObject(forKey: key)
-    }
-    
-    private func encode(_ token: AuthToken) -> Data? {
-        let encoder = JSONEncoder()
-        return try? encoder.encode(token)
-    }
-    
-    private func decode(_ data: Data) -> AuthToken? {
-        let decoder = JSONDecoder()
-        return try? decoder.decode(AuthToken.self, from: data)
-    }
+    func load() -> AuthToken?
+    func save(token: AuthToken)
+    func delete()
     
 }
